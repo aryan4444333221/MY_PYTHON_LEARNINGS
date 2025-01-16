168a37d8b4acccc70cb43d53b36f1ce786f69a1e
aryan4444333221 <aryan4444333221@gmail.com>
2025-01-16 07:09:11 +0530

    Adding Critical Analysis On Numbers

---
+++ playing_with_numbers.py
@@ -0,0 +1,127 @@
+#CASE1 both +ve
+# a = 50
+# b = 33
+# print(a -b)
+# c = a - b
+# print(c)
+# print(a * b)
+# print(a / b)
+# d = a * b
+# print(d)
+# d = a//b
+# print(d)
+
+#output
+# 17
+# 17
+# 1650
+# 1.5151515151515151
+# 1650
+# 1
+
+#CASE 2 both -ve
+# a = -22
+# b = -10
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+
+# output
+# -30
+# -10
+# 200
+# 2.0
+# 2
+
+#CASE 3: a +ve and b -ve
+# a = 29
+# b = -20
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+# output
+# 9
+# 49
+# -580
+# -1.45
+# -2
+
+#CASE 4: a -ve and b +ve
+# a = -29
+# b = 20
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+
+# output
+# -9
+# -49
+# -580
+# -1.45
+# -2
+
+#FLOAT CASES
+#both are positive
+# a = 5.3
+# b = 3.3
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % 2)
+
+# output 
+# 8.7
+# 1.9
+# 18.02
+# 1.5588235294117647
+# 1.0
+#both are negative
+# a = -5.3
+# b = -8.7
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % 2)
+
+# output
+# -14.0
+# 3.3999999999999995
+# 46.10999999999999
+# 0.6091954022988506
+# 0.0
+# 0.7000000000000002
+
+# one is negative other is positive
+a = -4.3
+b = 3.7
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % 2)
+#output
+# -0.5999999999999996
+# -8.0
+# -15.91
+# -1.162162162162162
+# -2.0
+# 1.7000000000000002
+# a = 10
+
+# print(a, "is the value")
+
+# '''a''' - multi line comment
