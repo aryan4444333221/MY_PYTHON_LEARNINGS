55dd1e42ae4fa22426b4ee41e209733255355dff
aryan4444333221 <aryan4444333221@gmail.com>
2025-01-18 16:39:30 +0530

    Edited some Stuff

--- first.py
+++ first.py
@@ -1,7 +1,9 @@
-print("Hello World") # First Python Code 
+# First Python Code 
+print("Hello World") 
 
+# Writing first line of code
 print("This is my First Code")
 
-#Lets add a new line of code
+# Lets add a new line of code
 
 print("Just adding a new line of code to test github")
